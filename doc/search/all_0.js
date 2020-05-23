c4a1c6e1c27c95e1179dd7451f8d91bcbcd89f63
Marcelina Woziwoda <59450291+marcelinawoziwoda@users.noreply.github.com>
2020-05-23 14:34:40 +0200

    master-clear

--- doc/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['hascometotheend_2eino_0',['hascometotheend.ino',['../hascometotheend_8ino.html',1,'']]],
-  ['hasrun_1',['hasRun',['../hascometotheend_8ino.html#a73eec6512b0713ab8c234e1e67c63668',1,'hascometotheend.ino']]]
-];
--- doc/search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['user_13',['user',['../hascometotheend_8ino.html#ac674ad976fca91dfcd58cf864228b9a0',1,'hascometotheend.ino']]]
-];
